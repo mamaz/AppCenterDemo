c41168e1c091d290fd870a64cd7829a37bc058d6
mamaz <hisma.sudradjat@btpn.com>
2018-02-19 15:22:34 +0700

    Initial Commit

---
+++ AppCenterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppCenterDemo
+//
+//  Created by Hisma Mulya S on 2/19/18.
+//  Copyright © 2018 BTPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppCenterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppCenterDemo
+//
+//  Created by Hisma Mulya S on 2/19/18.
+//  Copyright © 2018 BTPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
